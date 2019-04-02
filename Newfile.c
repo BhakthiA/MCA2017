71380909222a3d9b0e78fe9ae8ad08a5ca1a91a8
BhakthiA <bhakthipatel07@gmail.com>
2019-04-02 14:34:56 +0530

    New Program First commit

---
+++ Newfile.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("Enter the values of a and b:");
+scanf("%d%d",&a,&b);
+c=a+b;
+printf("\nSum of two numbers:%d",c);
+getch();
+}
