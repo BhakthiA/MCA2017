562f5b87f90b9f76d10bfdf9fc4f2d66bb1f7c8a
BhakthiA <bhakthipatel07@gmail.com>
2019-05-07 15:17:30 +0530

    Program in HEAVEN

---
+++ HEAVEN.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+clrscr()
+int a,b;
+printf("Enter the numbers to be Swap");
+printf("BEFORE SWAP");
+scanf("%d%d",&a,$b);
+temp=$a;
+a=&b;
+b=&temp;
+printf("AFTER SWAP");
+printf("a=&a and b=&b");
+getch();
+}
